aa1650e4149f0d94d9fc8e7c7fda419f5170a1f3
neohere97 <shalawadi.chinmay@gmail.com>
2019-01-09 02:15:56 +0530

    Added Range splits

--- Master/app.py
+++ Master/app.py
@@ -3,6 +3,8 @@ import time
 import httplib2
 import requests
 import threading
+import json
+import hashlib
 from flask import Flask,request
 
 app = Flask(__name__)
@@ -11,32 +13,39 @@ app = Flask(__name__)
 
 @app.route('/runPrimeFivePi', methods=['POST'])
 def FivePrime():
-    t= threading.Thread(target=PrimeFive)
+    data = json.loads(request.data.decode("utf-8"))    
+    range_splits = splitRange(data["Upper Limit"],data["Lower Limit"],20)    
+    t= threading.Thread(target=PrimeFive,args=(range_splits,))
     t.start()
     return "OK"
 
 
 @app.route('/runPrimeOnePi', methods=['POST'])
 def OnePrime():
-    t= threading.Thread(target=PrimeOne)
+    data = json.loads(request.data.decode("utf-8"))    
+    range_splits = splitRange(data["Upper Limit"],data["Lower Limit"],4)    
+    t= threading.Thread(target=PrimeOne,args=(range_splits,))
     t.start()            
     return "OK"
 
 @app.route('/runBruteFivePi', methods=['POST'])
 def FiveBrute():
-    t= threading.Thread(target=BruteFive)
+    data = json.loads(request.data.decode("utf-8"))    
+    crypt_msg = encrypt(data['Message'],data['Password'])    
+    t= threading.Thread(target=BruteFive,args=(crypt_msg,))
     t.start()
     return "OK"
 
 @app.route('/runBruteOnePi', methods=['POST'])
-def FiveBrute():
-    t= threading.Thread(target=BruteOne)
+def OneBrute():
+    data = json.loads(request.data.decode("utf-8"))    
+    crypt_msg = encrypt(data['Message'],data['Password'])    
+    t= threading.Thread(target=BruteOne,args=(crypt_msg,))
     t.start()
     return "OK"
     
 
-def PrimeFive():
-    t1 = time.time()
+def PrimeFive(range_splits):    
 
     k1 = rpyc.connect('192.168.2.101',18861)
     l1 = rpyc.connect('192.168.2.102',18861)
@@ -64,32 +73,32 @@ def PrimeFive():
     n4 = rpyc.connect('192.168.2.104',18864)
     o4 = rpyc.connect('192.168.2.105',18864)
 
-    k11 = rpyc.async_(k1.root.no_of_prime_numbers)(25000,0)
-    k22 = rpyc.async_(k2.root.no_of_prime_numbers)(33000,25000)
-    k33 = rpyc.async_(k3.root.no_of_prime_numbers)(40000,33000)
-    k44 = rpyc.async_(k4.root.no_of_prime_numbers)(45000,40000)
+    k11 = rpyc.async_(k1.root.no_of_prime_numbers)(range_splits[0][1],range_splits[0][0])
+    k22 = rpyc.async_(k2.root.no_of_prime_numbers)(range_splits[1][1],range_splits[1][0]+1)
+    k33 = rpyc.async_(k3.root.no_of_prime_numbers)(range_splits[2][1],range_splits[2][0]+1)
+    k44 = rpyc.async_(k4.root.no_of_prime_numbers)(range_splits[3][1],range_splits[3][0]+1)
 
-    l11 = rpyc.async_(l1.root.no_of_prime_numbers)(51000,45000)
-    l22 = rpyc.async_(l2.root.no_of_prime_numbers)(55000,51000)
-    l33 = rpyc.async_(l3.root.no_of_prime_numbers)(59000,55000)
-    l44 = rpyc.async_(l4.root.no_of_prime_numbers)(63000,59000)
+    l11 = rpyc.async_(l1.root.no_of_prime_numbers)(range_splits[4][1],range_splits[4][0]+1)
+    l22 = rpyc.async_(l2.root.no_of_prime_numbers)(range_splits[5][1],range_splits[5][0]+1)
+    l33 = rpyc.async_(l3.root.no_of_prime_numbers)(range_splits[6][1],range_splits[6][0]+1)
+    l44 = rpyc.async_(l4.root.no_of_prime_numbers)(range_splits[7][1],range_splits[7][0]+1)
 
-    m11 = rpyc.async_(m1.root.no_of_prime_numbers)(67000,63000)
-    m22 = rpyc.async_(m2.root.no_of_prime_numbers)(70000,67000)
-    m33 = rpyc.async_(m3.root.no_of_prime_numbers)(72000,70000)
-    m44 = rpyc.async_(m4.root.no_of_prime_numbers)(74000,72000)
+    m11 = rpyc.async_(m1.root.no_of_prime_numbers)(range_splits[8][1],range_splits[8][0]+1)
+    m22 = rpyc.async_(m2.root.no_of_prime_numbers)(range_splits[9][1],range_splits[9][0]+1)
+    m33 = rpyc.async_(m3.root.no_of_prime_numbers)(range_splits[10][1],range_splits[10][0]+1)
+    m44 = rpyc.async_(m4.root.no_of_prime_numbers)(range_splits[11][1],range_splits[11][0]+1)
 
-    n11 = rpyc.async_(n1.root.no_of_prime_numbers)(77000,74000)
-    n22 = rpyc.async_(n2.root.no_of_prime_numbers)(81000,77000)
-    n33 = rpyc.async_(n3.root.no_of_prime_numbers)(85000,81000)
-    n44 = rpyc.async_(n4.root.no_of_prime_numbers)(88000,85000)
+    n11 = rpyc.async_(n1.root.no_of_prime_numbers)(range_splits[12][1],range_splits[12][0]+1)
+    n22 = rpyc.async_(n2.root.no_of_prime_numbers)(range_splits[13][1],range_splits[13][0]+1)
+    n33 = rpyc.async_(n3.root.no_of_prime_numbers)(range_splits[14][1],range_splits[14][0]+1)
+    n44 = rpyc.async_(n4.root.no_of_prime_numbers)(range_splits[15][1],range_splits[15][0]+1)
 
-    o11 = rpyc.async_(o1.root.no_of_prime_numbers)(92000,88000)
-    o22 = rpyc.async_(o2.root.no_of_prime_numbers)(95500,92000)
-    o33 = rpyc.async_(o3.root.no_of_prime_numbers)(98000,95500)
-    o44 = rpyc.async_(o4.root.no_of_prime_numbers)(100000,98000)
-
-
+    o11 = rpyc.async_(o1.root.no_of_prime_numbers)(range_splits[16][1],range_splits[16][0]+1)
+    o22 = rpyc.async_(o2.root.no_of_prime_numbers)(range_splits[17][1],range_splits[17][0]+1)
+    o33 = rpyc.async_(o3.root.no_of_prime_numbers)(range_splits[18][1],range_splits[18][0]+1)
+    o44 = rpyc.async_(o4.root.no_of_prime_numbers)(range_splits[19][1],range_splits[19][0]+1)
+     
+    t1 = time.time()
     pi1 = k11.ready == False and k22.ready == False and k33.ready == False and k44.ready == False
     pi2 = l11.ready == False and l22.ready == False and l33.ready == False and l44.ready == False
     pi3 = m11.ready == False and m22.ready == False and m33.ready == False and m44.ready == False
@@ -107,27 +116,22 @@ def PrimeFive():
 
     result = f" Number of Prime Numbers : {pi1_sum + pi2_sum + pi3_sum + pi4_sum + pi5_sum} \n Time : {time.time() - t1} seconds "
     
-    http = httplib2.Http()    
-    response, content = http.request( 'http://127.0.0.1:1880/PrimeFiveResult',
-                                          'POST',
-                                          result
-                                      )
+    send_result('http://127.0.0.1:1880/PrimeFiveResult',result)
 
-def PrimeOne():
-    t1 = time.time()
-
+def PrimeOne(range_splits):   
+    
     k1 = rpyc.connect('192.168.2.101',18861)  
     k2 = rpyc.connect('192.168.2.101',18862) 
     k3 = rpyc.connect('192.168.2.101',18863)
     k4 = rpyc.connect('192.168.2.101',18864)
     
 
-    k11 = rpyc.async_(k1.root.no_of_prime_numbers)(45000,0)
-    k22 = rpyc.async_(k2.root.no_of_prime_numbers)(70000,45000)
-    k33 = rpyc.async_(k3.root.no_of_prime_numbers)(88000,70000)
-    k44 = rpyc.async_(k4.root.no_of_prime_numbers)(100000,88000)
+    k11 = rpyc.async_(k1.root.no_of_prime_numbers)(range_splits[0][1],range_splits[0][0])
+    k22 = rpyc.async_(k2.root.no_of_prime_numbers)(range_splits[1][1],range_splits[1][0]+1)
+    k33 = rpyc.async_(k3.root.no_of_prime_numbers)(range_splits[2][1],range_splits[2][0]+1)
+    k44 = rpyc.async_(k4.root.no_of_prime_numbers)(range_splits[3][1],range_splits[3][0]+1)
     
-
+    t1 = time.time()
     pi1 = k11.ready == False and k22.ready == False and k33.ready == False and k44.ready == False
  
 
@@ -140,13 +144,150 @@ def PrimeOne():
 
     result = f"Number Of Prime Numbers: {pi1_sum} \n Time : {time.time() - t1} seconds   "    
     
+    send_result('http://127.0.0.1:1880/PrimeOneResult',result)
+    
+
+
+def BruteFive(crypt_msg):     
+    k1 = rpyc.connect('192.168.2.101',18861)
+    l1 = rpyc.connect('192.168.2.102',18861)
+    m1 = rpyc.connect('192.168.2.103',18861)
+    n1 = rpyc.connect('192.168.2.104',18861)
+    o1 = rpyc.connect('192.168.2.105',18861)
+
+    k2 = rpyc.connect('192.168.2.101',18862)
+    l2 = rpyc.connect('192.168.2.102',18862)
+    m2 = rpyc.connect('192.168.2.103',18862)
+    n2 = rpyc.connect('192.168.2.104',18862)
+    o2 = rpyc.connect('192.168.2.105',18862)
+
+
+    k3 = rpyc.connect('192.168.2.101',18863)
+    l3 = rpyc.connect('192.168.2.102',18863)
+    m3 = rpyc.connect('192.168.2.103',18863)
+    n3 = rpyc.connect('192.168.2.104',18863)
+    o3 = rpyc.connect('192.168.2.105',18863)
+
+
+    k4 = rpyc.connect('192.168.2.101',18864)
+    l4 = rpyc.connect('192.168.2.102',18864)
+    m4 = rpyc.connect('192.168.2.103',18864)
+    n4 = rpyc.connect('192.168.2.104',18864)
+    o4 = rpyc.connect('192.168.2.105',18864)
+
+    k11 = rpyc.async_(k1.root.decrypt)(crypt_msg,500,0)
+    k22 = rpyc.async_(k2.root.decrypt)(crypt_msg,1000,500)
+    k33 = rpyc.async_(k3.root.decrypt)(crypt_msg,1500,1000)
+    k44 = rpyc.async_(k4.root.decrypt)(crypt_msg,2000,1500)
+
+    l11 = rpyc.async_(l1.root.decrypt)(crypt_msg,2500,2000)
+    l22 = rpyc.async_(l2.root.decrypt)(crypt_msg,3000,2500)
+    l33 = rpyc.async_(l3.root.decrypt)(crypt_msg,3500,3000)
+    l44 = rpyc.async_(l4.root.decrypt)(crypt_msg,4000,3500)
+
+    m11 = rpyc.async_(m1.root.decrypt)(crypt_msg,4500,4000)
+    m22 = rpyc.async_(m2.root.decrypt)(crypt_msg,5000,4500)
+    m33 = rpyc.async_(m3.root.decrypt)(crypt_msg,5500,5000)
+    m44 = rpyc.async_(m4.root.decrypt)(crypt_msg,6000,5500)
+
+    n11 = rpyc.async_(n1.root.decrypt)(crypt_msg,6500,6000)
+    n22 = rpyc.async_(n2.root.decrypt)(crypt_msg,7000,6500)
+    n33 = rpyc.async_(n3.root.decrypt)(crypt_msg,7500,7000)
+    n44 = rpyc.async_(n4.root.decrypt)(crypt_msg,8000,7500)
+
+    o11 = rpyc.async_(o1.root.decrypt)(crypt_msg,8500,8000)
+    o22 = rpyc.async_(o2.root.decrypt)(crypt_msg,9000,8500)
+    o33 = rpyc.async_(o3.root.decrypt)(crypt_msg,9500,9000)
+    o44 = rpyc.async_(o4.root.decrypt)(crypt_msg,10000,9500)
+
+    workers = [[k11,k1],[k22,k2],[k33,k3],[k44,k4],[l11,l1],[l22,l2],[l33,l3],[l44,l4],[m11,m1],[m22,m2],[m33,m3],[m44,m4],[n11,n1],[n22,n2],[n33,n3],[n44,n4],[o11,o1],[o22,o2],[o33,o3],[o44,o4]]
+    passowrd = 0
+    t1 = time.time()
+    flag = False
+    while True:
+        for i in range(0,20):
+            if(workers[i][0].ready == True and workers[i][0].value[0] == True):
+                passowrd = workers[i][0].value[1]                
+                for i in workers:
+                    i[1].close()
+                flag = True
+        if(flag):
+            break
+    result = f"Passowrd Found: {passowrd}....... Time Taken : {time.time()-t1}"    
+    send_result('http://127.0.0.1:1880/DecryptFiveResult',result)
+
+
+def BruteOne(crypt_msg):
+    
+    
+
+    k1 = rpyc.connect('192.168.2.101',18861)  
+    k2 = rpyc.connect('192.168.2.101',18862) 
+    k3 = rpyc.connect('192.168.2.101',18863)
+    k4 = rpyc.connect('192.168.2.101',18864)
+    
+    k11 = rpyc.async_(k1.root.decrypt)(crypt_msg,2500,0)
+    k22 = rpyc.async_(k2.root.decrypt)(crypt_msg,5000,2500)
+    k33 = rpyc.async_(k3.root.decrypt)(crypt_msg,7500,5000)
+    k44 = rpyc.async_(k4.root.decrypt)(crypt_msg,10000,7500)
+
+    workers = [[k11,k1],[k22,k2],[k33,k3],[k44,k4]]
+    flag = False
+    t1 = time.time()
+    while True:
+        for i in range(0,4):
+            if(workers[i][0].ready == True and workers[i][0].value[0] == True):
+                passowrd = workers[i][0].value[1]
+
+                for i in workers:
+                    i[1].close()
+                flag = True
+        if(flag):
+            break
+    result = f"Passowrd Found: {passowrd}...... Time Taken : {time.time()-t1}"
+    
+    print(result)
+    send_result('http://127.0.0.1:1880/DecryptOneResult',result)
+
+def send_result(http_url,result):
     http = httplib2.Http()
-    response, content = http.request( 'http://127.0.0.1:1880/PrimeOneResult',
-                                          'POST',
-                                          result) 
+    http.request( http_url,'POST',result) 
 
+def encrypt(msg,psw):        
+    print(f"{msg}{psw}")
+    msg_for_hash = msg.encode()
+    hash_code = hashlib.sha1(msg_for_hash).hexdigest()
+    enc_msg = []
+    for i in range(len(msg)):
+        enc_msg.append(chr(ord(msg[i]) + int((psw[i % len(psw)]))))
+    enc_msg.append(hash_code)
+    return enc_msg
+def splitRange(lower_limit,upper_limit,num_of_splits):    
+    cycles = 0    
+    ranges = []
+    k = 0        
+    for i in range(int(lower_limit),int(upper_limit)+1):
+        cycles = cycles + i
 
-def BruteOne():
+    equalized_cycles = int(cycles/num_of_splits)
+
+    split_cycles = 0
+    lower_limit_range = 0
+    upper_limit_range = 0
+    for i in range(int(lower_limit),int(upper_limit)+1):    
+        split_cycles = split_cycles + i
+        if(split_cycles >= equalized_cycles):
+            upper_limit_range = i
+            ranges.append([lower_limit_range,upper_limit_range])        
+            lower_limit_range = i
+            split_cycles = 0
+            k = k+1
+
+        if(k+1 == num_of_splits):
+            ranges.append([lower_limit_range,int(upper_limit)])
+            break
+    return ranges
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
